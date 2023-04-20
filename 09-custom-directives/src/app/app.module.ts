6a791c8f527c10d955acc17840f9668da526ff6b
porwalarchit <architporwal@gmail.com>
2023-04-21 01:18:54 +0530

    Added highlight directive..

--- 09-custom-directives/src/app/app.module.ts
+++ 09-custom-directives/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { setBackgroundDirective } from './CustomDirective/setbackground.directive';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    setBackgroundDirective
+    setBackgroundDirective,
+    HighlightDirective
   ],
   imports: [
     BrowserModule
