111f2d99158b202bba24eb3c9a481f0bdfc8053c
porwalarchit <architporwal@gmail.com>
2023-04-21 01:48:05 +0530

    @HostBinding example done.

--- 09-custom-directives/src/app/CustomDirective/better-highlight.directive.ts
+++ 09-custom-directives/src/app/CustomDirective/better-highlight.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective {
 
-  constructor() { }
+  constructor(private element: ElementRef, private renderer: Renderer2) { }
 
+  @HostBinding('style.backgroundColor') background: string = 'transparent';
+  @HostBinding('style.border') border: string = 'none';
+
+  @HostListener('mouseenter') mouseEnter(){
+    this.background = 'pink',
+    this.border = 'red 2px solid'
+  }
+
+  @HostListener('mouseleave') mouseLeave(){
+    this.background = 'transparent',
+    this.border = 'none0'
+  }
 }
