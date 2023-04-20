b317c9bcd88d6512d89452e64951363f78ec4e64
porwalarchit <architporwal@gmail.com>
2023-04-20 15:32:52 +0530

    @ViewChild decorator done.

--- 04-view-child/src/app/app.component.ts
+++ 04-view-child/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component} from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
+import { DemoComponent } from './demo/demo.component';
+
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,20 @@ import { Component} from '@angular/core';
 export class AppComponent {
   title = 'ViewChild';
 
+  @ViewChild('dobInput') dateOfBirth: ElementRef;
+  @ViewChild('ageInput') age: ElementRef;
+  @ViewChild(DemoComponent, {static: true}) demoComp : DemoComponent;
+
+  // Method to calcualate age, based on the DoB entered.
+  calculateAge(){
+    let birthYear = new Date(this.dateOfBirth.nativeElement.value).getFullYear();
+    let currentYear = new Date().getFullYear();
+
+    let age = currentYear-birthYear;
+
+    // Assign calculated age to the local variable.
+    this.age.nativeElement.value = age;
+
+    // console.log(this.dateOfBirth,'/n' ,this.age)
+  }
 }
