fa1e82588ddea38575228e77a49b43fb8e49d8fe
porwalarchit <architporwal@gmail.com>
2023-04-21 01:28:41 +0530

    Renderer2 example dome.

--- 09-custom-directives/src/app/highlight.directive.ts
+++ 09-custom-directives/src/app/highlight.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private element: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit(){
+    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', '#F1948A');
+    this.renderer.addClass(this.element.nativeElement, 'container');
+    this.renderer.setAttribute(this.element.nativeElement, 'title', "This is example div");
+  }
 
 }
