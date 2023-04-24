9afff76e2bf2fa3f1e94c8c7df6c4c60ad8ca297
porwalarchit <architporwal@gmail.com>
2023-04-24 18:43:29 +0530

    Learned different ways of creating observables..

--- 14-angular-observables/src/app/app.component.ts
+++ 14-angular-observables/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,27 +9,55 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit{
   title = 'angular-observables';
 
-  myObservable = new Observable((observer)=>{
-    console.log('Observable starts');
-    setTimeout(()=>{observer.next("1")}, 1000);
-    setTimeout(()=>{observer.next("2")}, 2000);
-    setTimeout(()=>{observer.next("3")}, 3000);
+  // WAY 1- Using Observable Constructor.
+  // myObservable = new Observable((observer)=>{
+  //   console.log('Observable starts');
+  //   setTimeout(()=>{observer.next("1")}, 1000);
+  //   setTimeout(()=>{observer.next("2")}, 2000);
+  //   setTimeout(()=>{observer.next("3")}, 3000);
     
-    // Create an Error.
-    // setTimeout(()=>{observer.error(new Error ('Something went wrong! Please try again later.'))}, 3000);
+  //   // Create an Error.
+  //   // setTimeout(()=>{observer.error(new Error ('Something went wrong! Please try again later.'))}, 3000);
 
-    setTimeout(()=>{observer.next("4")}, 4000);
-    setTimeout(()=>{observer.next("5")}, 5000);
+  //   setTimeout(()=>{observer.next("4")}, 4000);
+  //   setTimeout(()=>{observer.next("5")}, 5000);
 
-    // Emitting a complete signal.
-    setTimeout(()=>{observer.complete()}, 6000);
+  //   // Emitting a complete signal.
+  //   setTimeout(()=>{observer.complete()}, 6000);
 
-    // observer.next("1");
-    // observer.next("2");
-    // observer.next("3");
-    // observer.next("4");
-    // observer.next("5");
-  });
+  //   // observer.next("1");
+  //   // observer.next("2");
+  //   // observer.next("3");
+  //   // observer.next("4");
+  //   // observer.next("5");
+  // });
+
+  
+  // WAY 2- Using CREATE METHOD
+//   myObservable = Observable.create((observer: {
+//     complete: any; next: (arg0: string) => void; error: (arg0: Error) => void; 
+// })=>{
+//     setTimeout(()=>{observer.next("A")}, 1000);
+//     setTimeout(()=>{observer.next("B")}, 2000);
+//     setTimeout(()=>{observer.next("C")}, 3000);
+//     setTimeout(()=>{observer.next("D")}, 4000);
+    
+//     // Error
+//     // setTimeout(()=>{observer.error(new Error ('Something went wrong! Please try again later.'))}, 4000);
+//     setTimeout(()=>{observer.next("E")}, 5000);
+    
+//     // Complete signal
+//     setTimeout(()=>{observer.complete()}, 6000);
+//   });
+  array1 = [1,2,3,4,5];
+  array2 = ['A', 'B', 'C'];
+  
+  // WAY 3- Using of Operator
+  // myObservable = of(this.array1, this.array2, 20, 48.24, "Hello");
+
+  // WAY 4- USING FROM operator.
+  myObservable = from(this.array1);
+  
 
   ngOnInit(){
     this.myObservable.subscribe((val)=>{
