331fed2e83d3df474647f9e2b91d3ba87866709c
porwalarchit <architporwal@gmail.com>
2023-04-24 10:45:44 +0530

    Starting Services concept in Angular (basic setup of project done).

---
+++ 11-subscribe-service/src/app/angular/angular.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-angular',
+  templateUrl: './angular.component.html',
+  styleUrls: ['./angular.component.css']
+})
+export class AngularComponent{
+  title="Angular";
+}
---
+++ 11-subscribe-service/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { JavascriptComponent } from './javascript/javascript.component';
+import { AngularComponent } from './angular/angular.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    JavascriptComponent,
+    AngularComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
