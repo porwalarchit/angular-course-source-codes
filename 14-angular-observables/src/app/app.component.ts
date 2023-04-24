f0cd8bc9fed0ffa7b7254bb62c32de19859ba13c
porwalarchit <architporwal@gmail.com>
2023-04-24 17:57:33 +0530

    Complete signal of observables demo done.

--- 14-angular-observables/src/app/app.component.ts
+++ 14-angular-observables/src/app/app.component.ts
@@ -11,15 +11,18 @@ export class AppComponent implements OnInit{
 
   myObservable = new Observable((observer)=>{
     console.log('Observable starts');
-    setTimeout(()=>{observer.next("1");}, 1000);
-    setTimeout(()=>{observer.next("2");}, 2000);
-    setTimeout(()=>{observer.next("3");}, 3000);
+    setTimeout(()=>{observer.next("1")}, 1000);
+    setTimeout(()=>{observer.next("2")}, 2000);
+    setTimeout(()=>{observer.next("3")}, 3000);
     
     // Create an Error.
-    setTimeout(()=>{observer.error(new Error ('Something went wrong! Please try again later.'))}, 3000);
+    // setTimeout(()=>{observer.error(new Error ('Something went wrong! Please try again later.'))}, 3000);
 
-    setTimeout(()=>{observer.next("4");}, 4000);
-    setTimeout(()=>{observer.next("5");}, 5000);
+    setTimeout(()=>{observer.next("4")}, 4000);
+    setTimeout(()=>{observer.next("5")}, 5000);
+
+    // Emitting a complete signal.
+    setTimeout(()=>{observer.complete()}, 6000);
 
     // observer.next("1");
     // observer.next("2");
@@ -33,6 +36,8 @@ export class AppComponent implements OnInit{
       console.log(val);
     }, (error)=>{
       console.log(error.message);
+    }, ()=>{
+      console.log("Observables has completed emitting all values.")
     });
   }
 }
