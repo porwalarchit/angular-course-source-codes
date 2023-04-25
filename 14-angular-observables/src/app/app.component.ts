c31ea49c43740ce605ec47dce354043b2d52a669
porwalarchit <architporwal@gmail.com>
2023-04-25 10:28:55 +0530

    RxJS operators such as map() and filter() examples done.

--- 14-angular-observables/src/app/app.component.ts
+++ 14-angular-observables/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, from, of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'angular-observables';
 
   // WAY 1- Using Observable Constructor.
@@ -15,7 +16,7 @@ export class AppComponent implements OnInit{
   //   setTimeout(()=>{observer.next("1")}, 1000);
   //   setTimeout(()=>{observer.next("2")}, 2000);
   //   setTimeout(()=>{observer.next("3")}, 3000);
-    
+
   //   // Create an Error.
   //   // setTimeout(()=>{observer.error(new Error ('Something went wrong! Please try again later.'))}, 3000);
 
@@ -32,39 +33,54 @@ export class AppComponent implements OnInit{
   //   // observer.next("5");
   // });
 
-  
+
   // WAY 2- Using CREATE METHOD
-//   myObservable = Observable.create((observer: {
-//     complete: any; next: (arg0: string) => void; error: (arg0: Error) => void; 
-// })=>{
-//     setTimeout(()=>{observer.next("A")}, 1000);
-//     setTimeout(()=>{observer.next("B")}, 2000);
-//     setTimeout(()=>{observer.next("C")}, 3000);
-//     setTimeout(()=>{observer.next("D")}, 4000);
-    
-//     // Error
-//     // setTimeout(()=>{observer.error(new Error ('Something went wrong! Please try again later.'))}, 4000);
-//     setTimeout(()=>{observer.next("E")}, 5000);
-    
-//     // Complete signal
-//     setTimeout(()=>{observer.complete()}, 6000);
-//   });
-  array1 = [1,2,3,4,5];
+  //   myObservable = Observable.create((observer: {
+  //     complete: any; next: (arg0: string) => void; error: (arg0: Error) => void; 
+  // })=>{
+  //     setTimeout(()=>{observer.next("A")}, 1000);
+  //     setTimeout(()=>{observer.next("B")}, 2000);
+  //     setTimeout(()=>{observer.next("C")}, 3000);
+  //     setTimeout(()=>{observer.next("D")}, 4000);
+
+  //     // Error
+  //     // setTimeout(()=>{observer.error(new Error ('Something went wrong! Please try again later.'))}, 4000);
+  //     setTimeout(()=>{observer.next("E")}, 5000);
+
+  //     // Complete signal
+  //     setTimeout(()=>{observer.complete()}, 6000);
+  //   });
+  array1 = [1, 2, 3, 4, 5];
   array2 = ['A', 'B', 'C'];
-  
+
   // WAY 3- Using of Operator
   // myObservable = of(this.array1, this.array2, 20, 48.24, "Hello");
 
   // WAY 4- USING FROM operator.
-  myObservable = from(this.array1);
-  
+  myObservable = from(this.array1)
+  .pipe(map((val) => {
+    return val * 5;
+  }), filter((val)=>{
+    return val>10;
+  })
+  );
 
-  ngOnInit(){
-    this.myObservable.subscribe((val)=>{
+  // Returns values multiplied by 5.
+  // multiplieddata = this.myObservable.pipe(map((val) => {
+  //   return val * 5;
+  // }))
+
+  // Returns filtered values that are greatar than 10.
+  // filtererddata = this.multiplieddata.pipe(filter((val)=>{
+  //   return val>10;
+  // }))
+
+  ngOnInit() {
+    this.myObservable.subscribe((val) => {
       console.log(val);
-    }, (error)=>{
+    }, (error) => {
       console.log(error.message);
-    }, ()=>{
+    }, () => {
       console.log("Observables has completed emitting all values.")
     });
   }
