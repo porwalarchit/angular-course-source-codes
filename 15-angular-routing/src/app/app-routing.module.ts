151da9a960b55d9a07052b471917ef2c9d0eb200
porwalarchit <architporwal@gmail.com>
2023-04-26 17:42:20 +0530

    Created a seperate app-routing module file that will handle all the routes that we are creating, removed the routing code from app.module.ts file.

---
+++ 15-angular-routing/src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseComponent } from './courses/course/course.component';
+import { ErrorComponent } from './error/error.component';
+
+const appRoute: Routes = [
+    { path: '', component: HomeComponent },
+    // {path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'courses', component: CoursesComponent },
+    // {path: 'courses/course/:id', component: CourseComponent},
+    {
+        path: 'courses', children: [
+            { path: 'course/:id', component: CourseComponent }
+        ]
+    },
+    { path: '**', component: ErrorComponent }
+]
+
+@NgModule({
+    imports:[
+        RouterModule.forRoot(appRoute)
+    ],
+    exports:[
+        RouterModule
+    ]
+
+})
+export class AppRoutingModule {
+
+}
