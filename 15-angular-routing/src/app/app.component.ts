09855c730422e2f872f975a471be75af612e5d77
porwalarchit <architporwal@gmail.com>
2023-04-27 15:34:04 +0530

    Added loading icon on navigating from one route to another, used Navigation events such as NavigationStart, NavigationEnd, NavigationError, NavigationCancel.

--- 15-angular-routing/src/app/app.component.ts
+++ 15-angular-routing/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Component({
@@ -9,15 +9,24 @@ import { AuthService } from './auth.service';
 })
 export class AppComponent {
   title = 'AngularRouting';
-  displayLoadingIndicator = true;
+  displayLoadingIndicator = false;
 
-  constructor(private activatedRoute: ActivatedRoute, private authService: AuthService){
+  constructor(private activatedRoute: ActivatedRoute, private authService: AuthService, private router: Router){
 
   }
 
   ngOnInit(){
     this.activatedRoute.fragment.subscribe((value)=>{
       this.jumpTo(value);
+    })
+
+    this.router.events.subscribe((routerEvent)=>{
+      if(routerEvent instanceof NavigationStart){
+        this.displayLoadingIndicator = true;
+      }
+      if(routerEvent instanceof NavigationEnd || routerEvent instanceof NavigationCancel || routerEvent instanceof NavigationError){
+        this.displayLoadingIndicator = false;
+      }
     })
   }
 
