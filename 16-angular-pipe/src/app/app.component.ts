66586a5b6407c521ad063b6efe443223b4646950
porwalarchit <architporwal@gmail.com>
2023-04-28 10:50:43 +0530

    Created a function in app.component.ts file that will render the changes that we are getting uing pure and impure pipe.

--- 16-angular-pipe/src/app/app.component.ts
+++ 16-angular-pipe/src/app/app.component.ts
@@ -8,28 +8,55 @@ import { StudentService } from './students.service';
   styleUrls: ['./app.component.css'],
   providers: [StudentService]
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'AngularPipes';
   students: Student[];
   totalMarks: number;
-  filterText: string = ''
+  _filterText: string = ''
+  filteredStudents: Student[];
 
-  constructor(private studentService: StudentService){
+  get filterText() {
+    return this._filterText;
+  }
+
+  set filterText(value: string) {
+    this._filterText = value;
+    this.filteredStudents = this.filterStudentByGender(value);
+  }
+
+  constructor(private studentService: StudentService) {
 
   }
-  ngOnInit(){
+  ngOnInit() {
     this.students = this.studentService.students;
     this.totalMarks = this.studentService.totalMarks;
+    this.filteredStudents = this.students;
   }
 
-  AddDummyStudent(){
-    let studentCopy = Object.assign([], this.students);
-    studentCopy.push({name:'TEST', course: 'TEST', marks: 520, DOB: new Date(), gender: 'female'});
-    this.students = studentCopy
+  AddDummyStudent() {
+    // let studentCopy = Object.assign([], this.students);
+    // studentCopy.push({name:'TEST', course: 'TEST', marks: 520, DOB: new Date(), gender: 'female'});
+    // this.students = studentCopy;
+    this.students.push({ name: 'TEST', course: 'TEST', marks: 520, DOB: new Date(), gender: 'female' });
+    this.filteredStudents = this.filterStudentByGender(this._filterText);
+
   }
-  changeGender(){
-    let studentCopy = Object.assign([], this.students);
-    studentCopy[0].gender = 'female';
-    this.students = studentCopy;
+  changeGender() {
+    // let studentCopy = Object.assign([], this.students);
+    // studentCopy[0].gender = 'female';
+    // this.students = studentCopy;
+    this.students[0].gender = 'female';
+    this.filteredStudents = this.filterStudentByGender(this._filterText);
+  }
+
+  filterStudentByGender(filterTerm: string) {
+    if (this.students.length === 0 || filterTerm === '') {
+      return this.students;
+    }
+    else {
+      return this.students.filter((student) => {
+        return student.gender.toLowerCase() === filterTerm.toLowerCase();
+      })
+    }
   }
 }
